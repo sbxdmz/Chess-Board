aa690ccf36ad95be43aa1c33b68a918e90c86ace
sbxdmz <106126276+sbxdmz@users.noreply.github.com>
2024-09-20 17:02:32 -0700

    Initial commit

---
+++ Assets/Scripts/Chess Game/PieceObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceObject : MonoBehaviour
+{
+    public Sprite blackSprite;
+    public Sprite whiteSprite;
+}
